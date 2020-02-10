f87ea719e4e81dda87de523b237df3d39b4034e8
Ben <bps27@cam.ac.uk>
2020-02-10 11:16:22 +0000

    code generalised, and diff function vectorised

--- Master.py
+++ Master.py
@@ -1,6 +1,8 @@
 #  PROGRAM DESCRIPTION~|| Simulates motion of a breaking spaceline
-#  VERSION~            || v1.2
-#  LAST EDITED~        || 07/02/2020
+#  VERSION~            || v1.3
+#  LAST EDITED~        || 10/02/2020
+
+#note currently doesnt quite work.... need to check diff function
 
 import numpy as np
 from scipy.integrate import odeint
@@ -15,13 +17,31 @@ omega = 2*np.pi/tem        # Earth-moon angular velocity around CoM
 earth_moon_dist = 3.84E8   # 3.84E8 m
 earth_mass = 5.972E24      # 5.972E24 kg
 moon_mass = 7.348E22       # 7.348E22 kg
+earth_radius = 6371000     # 6371000 kg
+moon_radius = 1731100      # 1731100 m
 G = 6.67E-11               # 6.67E-11 N m^2 kg^-2
 com_distance = earth_moon_dist * moon_mass / (earth_mass + moon_mass)
 
 # SIMULATION PARAMETERS
+mass = 100
+k = 100
 
+def init_basic(n, l, i):
+    """
+    Sets up input vector of the form [r_0, r_1, ..., r_n, dr_0/dt, dr_1/dt, ..., dr_n/dt], such that each spring is
+    un-extended and the line stretches l % of the way from the moon to the Earth. The line is cut between the ith and
+    i+1th mass
+    Note that in reality the line should be under tension and hence these initial conditions are wrong
+    :param n: number of masses
+    :param l: percentage of way the nth mass is from the moon centre to the Earth centre
+    :param i: the line is cut between the ith and i+1th mass
+    :return: vector as described above
+    """
+    ret = np.zeros(n*4)
+    for j in range(n):
+        ret[4*j] = earth_moon_dist - moon_radius - com_distance - i*(earth_moon_dist*l - moon_radius)/(n-1)
+        # all others remain 0
 
-# note - to start with - 1 mass in gravitational field of 2 bodies.
 
 def diff(inp, t):
     """
@@ -30,33 +50,54 @@ def diff(inp, t):
     :param t: Current time in simulation
     :return: The time derivative of the input vector
     """
-    r_e = np.sqrt((inp[0]+com_distance)**2 + inp[1]**2)                     # distance to earth
-    r_m = np.sqrt((inp[0]-earth_moon_dist+com_distance)**2 + inp[1]**2)     # distance to moon
+
+    positions = inp[:len(inp)//2]
+    velocities = inp[len(inp)//2:]
+
+    # distance to earth (correctly shaped)
+    r_from_earth = np.copy(positions)
+    r_from_earth[::2] += com_distance
+    tmp1 = r_from_earth ** 2
+    r_e = np.sqrt(np.repeat([sum(tmp1[i:i+2]) for i in range(0, len(tmp1), 2)], 2))
+
+    # distance to moon (correctly shaped)
+    r_from_moon = np.copy(positions)
+    r_from_moon[::2] += com_distance - earth_moon_dist
+    tmp1 = r_from_moon ** 2
+    r_m = np.sqrt(np.repeat([sum(tmp1[i:i+2]) for i in range(0, len(tmp1), 2)], 2))
+
     # Gravity
-    a_0 = -G*earth_mass*(inp[0]+com_distance)/(r_e**3) - G*moon_mass*(inp[0]-earth_moon_dist+com_distance)/(r_m**3)
-    a_1 = -G*earth_mass*inp[1]/(r_e**3) - G*moon_mass*(inp[1])/(r_m**3)
-    # Coriolis
-    a_0 += -2*omega*inp[3]
-    a_1 += 2*omega*inp[2]
+    a = -G*earth_mass*r_from_earth/(r_e**3) - G*moon_mass*r_from_moon/(r_m**3)
+
     # Centrifugal
-    a_0 += omega**2*inp[0]
-    a_1 += omega**2*inp[1]
-    ret = [inp[2], inp[3], a_0, a_1]
+    a += omega**2 * positions
+
+    # Coriolis
+    tmp = np.zeros(len(velocities))
+    tmp[::2] = -velocities[1::2]
+    tmp[1::2] = velocities[::2]
+    a += 2 * omega * tmp
+
+    # Combine velocities and accelerations
+    ret = np.concatenate([velocities, a])
     return ret
 
 
-def simulate():
+def simulate(init, domain):
     """
     Core simulation routine, returns the time-integrated solution (trajectory) by calling odeint
+    :init: initial conditions vector of the form: [r_0, r_1, ..., r_n, dr_0/dt, dr_1/dt, ..., dr_n/dt]
     :return: Vector containing position and velocity of each mass at each time
     """
-    init = [1.92E8-com_distance, 0, 0, 1440.4+omega*(1.92E8-com_distance)] # would be circular orbit if moon wasn't there
-    domain = np.linspace(0, 837700*2, 10000)                               # set to about 2 orbits
-    solution = odeint(diff, init, domain, full_output=True)
-    return solution
+    return odeint(diff, init, domain, full_output=True)
 
 
-solution = simulate()
+# simulation
+domain = np.linspace(0, 837700*2, 10000)
+init = [1.92E8-com_distance, 0, 0, 1440.4+omega*(1.92E8-com_distance)]  # would be circular orbit if moon wasn't there
+solution = simulate(init, domain)
+
+# plot
 x0 = []
 x1 = []
 for i in range(len(solution[0])):
@@ -65,10 +106,9 @@ for i in range(len(solution[0])):
 plt.plot(x0, x1, color='k')
 
 # add earth and moon (centred at com)
-earth = plt.Circle((-com_distance, 0), 6371000, color='b')
-moon = plt.Circle((earth_moon_dist - com_distance, 0), 1731100, color='gray')
+earth = plt.Circle((-com_distance, 0), earth_radius, color='b')
+moon = plt.Circle((earth_moon_dist - com_distance, 0), moon_radius, color='gray')
 plt.gcf().gca().add_artist(earth)
 plt.gcf().gca().add_artist(moon)
 plt.xlim(-earth_moon_dist*0.75, earth_moon_dist*1.25)
-
 plt.show()
